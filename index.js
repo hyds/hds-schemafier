ccedb4e9ed6936bd6b510da07da89774c45f936e
Sholto Maud <sholto.maud@gmail.com>
2015-02-27 01:25:02 -0500

    fixing

--- index.js
+++ index.js
@@ -18,23 +18,10 @@ function Hydstra(options) {
   }
 
   // init Transform
-  util.inherits(Hydstra, Transform);
   Transform.call(this, options);
 }
 
-function NRMGWDB(options) {
-  // allow use without new
-  if (!(this instanceof NRMGWDB)) {
-    return new NRMGWDB(options);
-  }
-
-  // init Transform
-  util.inherits(NRMGWDB, Transform);
-  Transform.call(this, options);
-}
-
-NRMGWDB.prototype._transform = function (buf, enc, cb) {
-}
+util.inherits(Hydstra, Transform);
 
 Hydstra.prototype._transform = function (buf, enc, cb) {
     
@@ -73,107 +60,3 @@ Hydstra.prototype._transform = function (buf, enc, cb) {
     cb();
 };
 
-
-
-
-
-
-/*
-
-module.exports = { 
-	geofy : function geofy(buf){
-        var rs = new Readable;
-        rs.push('beep ');
-        rs.push('boop\n');
-        rs.push(null);
-
-
-        var input = through(write, end);
-        var geojson = {
-            "type": "FeatureCollection",
-        };
-        
-        function write (row) {
-        
-            var line = JSON.parse(buf.toString().replace(/;$/,""));
-            
-            console.log("line [",line,"]")
-
-            var sites = line._return.rows;
-            var features = [];
-            var feature = {
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "Point",
-                    }
-            };
-
-            for (var i = 0; i < sites.length; i++) {
-                var coordinates = [];
-                coordinates.push(sites[i].longitude);
-                coordinates.push(sites[i].latitude);
-                delete sites[i].longitude;
-                delete sites[i].latitude;
-                feature.geometry.coordinates = coordinates;
-                feature.properties = sites[i];
-                features.push(feature);
-            }   
-
-            geojson.features = features;
-
-            
-
-            
-
-        
-            
-        }
-        
-        function end () { console.log('geojson [',geojson,']'); }
-
-        return duplexer(input, geojson);
-
-    }
-}
-
-
-
-
-
-function geofyOld (buf) {
-    var line = JSON.parse(buf.toString().replace(/;$/,""));
-    
-    var sites = line._return.rows;
-    var features = [];
-    var feature = {
-            "type": "Feature",
-            "geometry": {
-                "type": "Point",
-            }
-    };
-
-    for (var i = 0; i < sites.length; i++) {
-        var coordinates = [];
-        coordinates.push(sites[i].longitude);
-        coordinates.push(sites[i].latitude);
-        delete sites[i].longitude;
-        delete sites[i].latitude;
-        feature.geometry.coordinates = coordinates;
-        feature.properties = sites[i];
-        features.push(feature);
-    }   
-
-    geojson.features = features;
-
-  	//console.log('geojson [',geojson,']');
-
-	return geojson;
-}
-
-/*
-{"station": "070048", "longitude": "149.44790000", "stname": "HOSKINTOWN RADIO
- OBSERVATORY (CBM)", "active": false, "stntype": "WEA", "elev": "1.910", "datemo
-d": 20111111, "latitude": "-35.36840000", "lldatum": "GDA94", "orgcode": "CBM"}
-
-
-*/
